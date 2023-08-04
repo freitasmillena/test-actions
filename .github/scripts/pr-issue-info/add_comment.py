11a23b3e41454f571efaf00f635bd7d92dd428cf
Millena Santos <freitasmillena27@gmail.com>
2023-08-04 14:02:36 +0100

    error handling

--- .github/scripts/pr-issue-info/add_comment.py
+++ .github/scripts/pr-issue-info/add_comment.py
@@ -4,22 +4,30 @@ import requests
 import json
 
 def add_comment(pr_number, repo, token, comment_filepath):
-    with open(comment_filepath, 'r') as file:
-        comment_body = file.read()
+    try:
+        with open(comment_filepath, 'r') as file:
+            comment_body = file.read()
 
-    url = f"https://api.github.com/repos/{repo}/issues/{pr_number}/comments"
-    headers = {"Authorization": f"token {token}", "Accept": "application/vnd.github.v3+json"}
-    payload = {"body": comment_body}
+        url = f"https://api.github.com/repos/{repo}/issues/{pr_number}/comments"
+        headers = {"Authorization": f"token {token}", "Accept": "application/vnd.github.v3+json"}
+        payload = {"body": comment_body}
 
-    response = requests.post(url, headers=headers, data=json.dumps(payload))
+        response = requests.post(url, headers=headers, data=json.dumps(payload))
 
-    if response.status_code != 201:
-        print("Failed to add comment")
-        print("Response:", response.text)
+        if response.status_code != 201:
+            print("Failed to add comment")
+            print("Response:", response.text)
+    except Exception as e:
+        print("An error occurred while adding the comment:")
+        print(str(e))
 
 if __name__ == "__main__":
-    pr_number = os.getenv('PR_NUMBER')
-    repo = os.getenv('GITHUB_REPOSITORY')
-    token = os.getenv('GITHUB_TOKEN')
-    comment_filepath = os.getenv('PATH')
-    add_comment(pr_number, repo, token, comment_filepath)
+    try:
+        pr_number = os.getenv('PR_NUMBER')
+        repo = os.getenv('GITHUB_REPOSITORY')
+        token = os.getenv('GITHUB_TOKEN')
+        comment_filepath = os.getenv('PATH')
+        add_comment(pr_number, repo, token, comment_filepath)
+    except Exception as e:
+        print("An error occurred while running the script:")
+        print(str(e))
