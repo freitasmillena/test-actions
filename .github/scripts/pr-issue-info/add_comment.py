6a69103a5af6d49c65f777788cc12d8832fdbab2
Millena Santos <freitasmillena27@gmail.com>
2023-08-04 14:13:04 +0100

    debug

--- .github/scripts/pr-issue-info/add_comment.py
+++ .github/scripts/pr-issue-info/add_comment.py
@@ -4,30 +4,23 @@ import requests
 import json
 
 def add_comment(pr_number, repo, token, comment_filepath):
-    try:
-        with open(comment_filepath, 'r') as file:
-            comment_body = file.read()
+    with open(comment_filepath, 'r') as file:
+        comment_body = file.read()
+        print(comment_body)
 
-        url = f"https://api.github.com/repos/{repo}/issues/{pr_number}/comments"
-        headers = {"Authorization": f"token {token}", "Accept": "application/vnd.github.v3+json"}
-        payload = {"body": comment_body}
+    url = f"https://api.github.com/repos/{repo}/issues/{pr_number}/comments"
+    headers = {"Authorization": f"token {token}", "Accept": "application/vnd.github.v3+json"}
+    payload = {"body": comment_body}
 
-        response = requests.post(url, headers=headers, data=json.dumps(payload))
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
 
-        if response.status_code != 201:
-            print("Failed to add comment")
-            print("Response:", response.text)
-    except Exception as e:
-        print("An error occurred while adding the comment:")
-        print(str(e))
+    if response.status_code != 201:
+        print("Failed to add comment")
+        print("Response:", response.text)
 
 if __name__ == "__main__":
-    try:
-        pr_number = os.getenv('PR_NUMBER')
-        repo = os.getenv('GITHUB_REPOSITORY')
-        token = os.getenv('GITHUB_TOKEN')
-        comment_filepath = os.getenv('PATH')
-        add_comment(pr_number, repo, token, comment_filepath)
-    except Exception as e:
-        print("An error occurred while running the script:")
-        print(str(e))
+    pr_number = os.getenv('PR_NUMBER')
+    repo = os.getenv('GITHUB_REPOSITORY')
+    token = os.getenv('GITHUB_TOKEN')
+    comment_filepath = os.getenv('PATH')
+    add_comment(pr_number, repo, token, comment_filepath)
